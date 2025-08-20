0d1f17e57860ab34ff4336b790c60da1a064f7e8
valle-maker <vallevalenciadiegoalejandro@gmail.com>
2025-08-20 13:34:55 -0500

    Añadi el método de buscar y mejore el de eliminar

--- Inventario.py
+++ Inventario.py
@@ -1,4 +1,4 @@
-#Voy en el segundo requerimiento 
+
 from Producto import Producto
 import csv
 class Inventario:
@@ -11,11 +11,37 @@ class Inventario:
         pro = Producto()
         pro.pedir_datos()
         self.productos.append(pro)
+    
+    #Método de busqueda
+    def buscar_productos(self, id):
+        pos = -1
+        for i in range(len(self.productos)):
+            if id == self.productos[i].id:
+                pos = i
+        if pos < 0:
+            print("No hay un prodcuto con ese id")
+        return pos
+
+
 
 
     #Proguntar si se pueden eliminar productos desde el archivo
     def eliminar_productos(self, id, cantidad):
-        self.productos.remove()
+        self.buscar_productos(id)#creo que la parte de poner la bandera para ver si se encontro se debe hacer el main
+        #Algo como si flag entonces preguntar por la cantidad que desea eliminar, seria hacer un condicional como, si inv.buscar_productos()==-1 flag=False...
+        #Lo hice en este método pero en el metodo de main tambien debo hacer algo parecido
+
+        #Hacer las validaciones, aparte de la de los tipos, tambien la de exitencias para eliminar 
+        if self.buscar_producto() >= 0:
+            self.lista[self.buscar_productos()].cantidad -= cantidad
+
+            
+        
+         
+
+
+
+        
 
 
 
@@ -23,4 +49,5 @@ class Inventario:
         
 obj=Inventario()
 obj.agregar_productos()
-
+obj.buscar_productos(123)
+#obj.eliminar_productos(123, 0)

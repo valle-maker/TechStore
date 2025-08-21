5d6c6f0a3cc6dfb3a15886f097b0b1280b45422e
valle-maker <vallevalenciadiegoalejandrogmail@.com>
2025-08-21 14:59:54 -0600

    Terminé el método eliminar y lo mejoré

--- Inventario.py
+++ Inventario.py
@@ -6,34 +6,51 @@ class Inventario:
         self.productos = []
         self.archivo_productos = "a_productos.csv"
 
-
+    #Hacer la verificación de que no se puede agregar cero productos o negativos
+    #Verificar que la lista no este vacia
     def agregar_productos(self):
         pro = Producto()
         pro.pedir_datos()
         self.productos.append(pro)
     
     #Método de busqueda
+    #Mejorar este método con lo de los index
+    
     def buscar_productos(self, id):
         pos = -1
         for i in range(len(self.productos)):
             if id == self.productos[i].id:
                 pos = i
-        if pos < 0:
-            print("No hay un prodcuto con ese id")
+        if pos <0:
+            print("No se encontro el producto")
+        
         return pos
 
 
 
 
-    #Proguntar si se pueden eliminar productos desde el archivo
+    #Preguntar si se pueden eliminar productos desde el archivo
+    
     def eliminar_productos(self, id, cantidad):
-        self.buscar_productos(id)#creo que la parte de poner la bandera para ver si se encontro se debe hacer el main
-        #Algo como si flag entonces preguntar por la cantidad que desea eliminar, seria hacer un condicional como, si inv.buscar_productos()==-1 flag=False...
-        #Lo hice en este método pero en el metodo de main tambien debo hacer algo parecido
+        posicion = self.buscar_productos(id)
 
-        #Hacer las validaciones, aparte de la de los tipos, tambien la de exitencias para eliminar 
-        if self.buscar_producto() >= 0:
-            self.lista[self.buscar_productos()].cantidad -= cantidad
+        if posicion >= 0:
+            if self.productos[posicion].cantidad>0:
+                if self.productosposicion[posicion].cantidad >= cantidad:
+                    self.productos[posicion].cantidad -= cantidad
+                else:
+                    print(f"Quiere eliminar {cantidad}, pero solo hay {self.productos[posicion].cantidad}")
+                    eliminar_todo = input("Eliminar todo (si/no)")
+                    if eliminar_todo.lower()=='si':
+                        self.productos[posicion].cantidad=0
+                        return
+                    else:
+                        return
+            else:
+                print("Ya no quedan existencias del producto a eliminar")
+        
+#Mirar que métodos siguen 
+#       
 
             
         
@@ -50,4 +67,4 @@ class Inventario:
 obj=Inventario()
 obj.agregar_productos()
 obj.buscar_productos(123)
-#obj.eliminar_productos(123, 0)
+obj.eliminar_productos(123, 8)

b9712c413f6afc6adacfeb2913521a532a4725db
valle-maker <vallevalenciadiegoalejandrogmail@.com>
2025-08-27 06:49:46 -0600

    Utilizi el método de de actulizar existencias en el de eliminar

--- Inventario.py
+++ Inventario.py
@@ -1,8 +1,10 @@
-#Tareas: 
-#Mirar si el método de mostrar los productos funciona
+#Tareas:
+#Actulizar precio o cantidada de producto existente 
 #Hacer la verificación de que no se puede agregar cero productos o negativos(Ya sea hacer el método o la clase)
 #Verificar que la lista no este vacia(Hacer que el método de verificacion funcione)
-#El método de busqueda funciona con pura lógica, deberia hacerlo mas profecional, posiblemente con métodos como index
+#Separar los inpus y los outputs
+#Mirar si se puede usar el método de actulizar_cantidad en el de eliminar por ejemplo
+#Para la parte del main, se guarda siempre un objeto, esto es para cuando lo este guardando en listas
 
 
 from Producto import Producto
@@ -17,6 +19,7 @@ class Inventario:
         pro = Producto()
         pro.pedir_datos()
         self.productos.append(pro)
+
     
     def verificar_lista_vacia(self):
         if self.productos == []:
@@ -24,10 +27,8 @@ class Inventario:
             return -1
         else:
             return 1
-    
-
-
-    
+        
+        
     def buscar_productos(self, id):
         verificacion_lista = self.verificar_lista_vacia()
         if verificacion_lista==1:
@@ -40,6 +41,7 @@ class Inventario:
             
             return pos
         
+        
     def buscar_productos_nombre(self, nombre):
         #Incluir la verificación de que la lista tenga datos
         for prod in self.productos: 
@@ -48,21 +50,19 @@ class Inventario:
             else:
                 print("No se encontro el producto")
 
+
     def mostrar_prodcutos(self):
         for pro in self.productos:
             print(f"Nombre:{pro.nombre}\nPrecio:{pro.precio}\nExistencias: {pro.cantidad}")
 
 
-
-
-    
     def eliminar_productos(self, id, cantidad):
         posicion = self.buscar_productos(id)
 
         if posicion >= 0:
             if self.productos[posicion].cantidad>0:
                 if self.productos[posicion].cantidad >= cantidad:
-                    self.productos[posicion].cantidad -= cantidad
+                    self.actulizar_cantidad(id, self.productos[posicion].cantidad-cantidad)
                 else:
                     print(f"Quiere eliminar {cantidad}, pero solo hay {self.productos[posicion].cantidad}")
                     eliminar_todo = input("Eliminar todo (si/no)")
@@ -73,11 +73,25 @@ class Inventario:
                         return
             else:
                 print("Ya no quedan existencias del producto a eliminar")
+
+    def actualizar_precio(self, id, n_precio):
+        pos = self.buscar_productos(id)
+        if pos >= 0:
+            self.productos[pos].precio = n_precio
+
+
+    def actulizar_cantidad(self, id, n_cantidad):
+        #Hacer las verificaciones de ingresar nuevas cantidades
+        pos = self.buscar_productos(id)
+        if pos >=0:
+            self.productos[pos].cantidad = n_cantidad
         
        
         
 obj=Inventario()
 obj.agregar_productos()
-obj.buscar_productos_nombre("dell")
+#obj.actualizar_precio(123, 3000)
+#obj.buscar_productos_nombre("dell")
 #obj.buscar_productos(123)
 obj.eliminar_productos(123, 8)
+obj.mostrar_prodcutos()
